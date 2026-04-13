a7431c29da64d44fe6009de98a42b75636b65709
phatt-23 <phat.trandai23@gmail.com>
2026-04-13 23:36:37 +0200

    compilation stops when lexer or parser has errors

--- compile.py
+++ compile.py
@@ -6,10 +6,24 @@ from pprint import pprint
 import antlr4
 from antlr.PJPLexer import PJPLexer
 from antlr.PJPParser import PJPParser
+from antlr4.error.ErrorListener import ErrorListener
 
 from trans import Transformer
 from check import Checker
 from gen import Generator
+
+
+class MyErrListener(ErrorListener):
+    def __init__(self):
+        super().__init__()
+        self.has_error = False
+        self.errs = []
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        self.has_error = True
+        recognizer_name = "lexer" if type(recognizer) == PJPLexer else "parser"
+        self.errs.append(f"[{line}:{column}] {recognizer_name} error - {msg}")
+
 
 GEN_DIR = "./gen/"
 
@@ -18,7 +32,9 @@ using_file = len(sys.argv) >= 2
 
 out_file = sys.argv[2] if len(sys.argv) >= 3 else None
 
+
 # get the input text from a file or stdin
+
 if using_file:
     file = sys.argv[1]
     with open(file) as f:
@@ -27,18 +43,38 @@ else:
     input_text = "".join(sys.stdin.readlines())
 
 print("INPUT:")
-print(input_text)
-print()
+print(input_text, end="\n\n", flush=True)
 
+
+# parse the read input
+
+err_listener = MyErrListener()
+
+print("ANTLR Lexing...")
 lexer = PJPLexer(antlr4.InputStream(input_text))
-tok_stream = antlr4.CommonTokenStream(lexer)
-parser = PJPParser(tok_stream)
+lexer.removeErrorListeners()
+lexer.addErrorListener(err_listener)
 
-tree = parser.prog()
+tok_stream = antlr4.CommonTokenStream(lexer)  # runs lexer
+
+print("ANTLR Parsing...")
+parser = PJPParser(tok_stream)
+parser.removeErrorListeners()
+parser.addErrorListener(err_listener)
+
+tree = parser.prog()  # runs parser
 
 print("PARSED INPUT:")
-print(tree.toStringTree(recog=parser))
-print()
+print(tree.toStringTree(recog=parser), end="\n\n", flush=True)
+
+
+if err_listener.has_error:
+    print("\n".join(err_listener.errs))
+    print("Parsing error occured. Exiting!")
+    sys.exit(0)
+
+
+# transform the antlr tree into my repr
 
 trans = Transformer(tok_stream)  
 
@@ -46,24 +82,28 @@ ast = trans.visit(tree)
 
 print("AST:")
 pprint(ast)
-print()
+print(flush=True)
+
+
+# type check
 
 checker = Checker()
 errors = checker.type_check(ast)
 
 print("TYPE CHECK:")
-print("\n".join(errors))
-print()
+print("\n".join(errors), end="\n\n", flush=True)
 
 if len(errors) != 0:
     sys.exit()
+
+
+# generate the stack code
 
 gen = Generator()
 out = gen.gen(ast)
 
 print("GENERATED:")
-print(out)
-print()
+print(out, end="\n\n", flush=True)
 
 if using_file:
     file = sys.argv[1]
